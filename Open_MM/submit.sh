05be902fff9364e69ddeac8c8de5f3db64abadf0
oliver-feighan <oliverfeighan1996@gmail.com>
2021-05-12 13:03:17 +0100

    run 2ns MD

--- Open_MM/submit.sh
+++ Open_MM/submit.sh
@@ -2,8 +2,8 @@
 #PBS -N openmm
 #PBS -o openmm.out
 #PBS -e openmm.err
-#PBS -l select=1:ncpus=24:mem=16G
-#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:ngpus=1:mem=16G
+#PBS -l walltime=0:50:00
 
 ENV_NAME="openmm"
 INPUT_SCRIPT="${PBS_O_WORKDIR}/run_workflow.py"
@@ -23,21 +23,27 @@ echo "  $(cat "${PBS_NODEFILE}" | uniq)"
 
 # Setup shell for conda
 # (based on conda initialize section of .bashrc, see e.g. https://erictleung.com/conda-in-subshell-script)
-CONDA_SETUP="$(conda shell.bash hook)"
-eval "${CONDA_SETUP}"
+#CONDA_SETUP="$(conda shell.bash hook)"
+#eval "${CONDA_SETUP}"
 
-conda activate ${ENV_NAME}
+module load lang/python/anaconda/3.8-2020.07
+
+#conda activate ${ENV_NAME}
+source activate ${ENV_NAME}
 
 echo "Conda env:  ${ENV_NAME}"
+
 echo "Python:     $(which python)"
-echo "Job script: ${ENTOS_SCRIPT}"
+echo "Job script: ${INPUT_SCRIPT}"
 echo "Result dir: ${RESULT_DIR}"
 
 echo -e "\n[Running job script]\n"
 
 mkdir -p ${RESULT_DIR}
 
-python ${INPUT_SCRIPT} ${PDB_PATH} ${XML_PATH} ${RESULT_DIR} -n ${NCPUS}
+#/home/of15641/.conda/envs/openmm/bin/python ${INPUT_SCRIPT} ${PDB_PATH} ${XML_PATH} ${RESULT_DIR} -n ${NCPUS}
+
+time python ${INPUT_SCRIPT} ${PDB_PATH} ${XML_PATH} ${RESULT_DIR} -o "2ns_run" -n ${NCPUS}
 
 echo -e "\n[Job script finished]\n"
 
