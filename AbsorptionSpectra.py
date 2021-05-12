bdd9391136feaf26cba3291797e7b2c08a2b43d8
Oliver-Feighan <oliverfeighan1996@gmail.com>
2021-05-12 14:24:54 +0100

    edit spectrum runner

--- AbsorptionSpectra.py
+++ AbsorptionSpectra.py
@@ -4,25 +4,16 @@ import subprocess
 import json
 import numpy as np
 import matplotlib.pyplot as plt
-
-def run_qcore(chrom):
-	qcore_path = os.environ["qcore_path"]
-
-	json_run = subprocess.run(f"{qcore_path} -f json -s \" res := bchla(structure({chrom.to_qcore_string()}))\"",
-		shell=True,
-		stdout=subprocess.PIPE,
-		executable="/bin/bash",
-		universal_newlines=True)
-
-	result = json.loads(json_run.stdout)["res"]
-
-	return result
+from concurrent.futures import ProcessPoolExecutor
 
 class Chromophore():
-	def __init__(self):
+	def __init__(self, name):
 		self.elem = []
 		self.xyz = []
 		self.n_atoms = 0
+		self.name = name
+		self.excitation_energy = None
+		self.transition_dipole = None
 
 	def to_qcore_string(self):
 		assert(len(self.elem) == len(self.xyz))
@@ -39,10 +30,42 @@ class Chromophore():
 
 		return result
 
+	def to_xyz_file(self):
+		assert(len(self.elem) == len(self.xyz))
+		file = open(f"frames/{self.name}.xyz", 'w')
+
+		print(f"{self.n_atoms}", file=file)
+		print(f"", file=file)
+
+		for i in range(self.n_atoms):
+			print(f"{self.elem[i]} {np.array2string(self.xyz[i], separator=' ')[1:-1]}", file=file)
+
+		file.close()
+
+
 	def append(self, elem : str, xyz : np.array):
 		self.elem.append(elem)
 		self.xyz.append(xyz)
 		self.n_atoms += 1
+
+class single_result():
+	def __init__(self, name, energy, transition_dipole):
+		self.name = name
+		self.energy = energy
+		self.transition_dipole = transition_dipole
+
+def run_qcore(chrom : Chromophore):
+	qcore_path = os.environ["qcore_path"]
+
+	json_run = subprocess.run(f"{qcore_path} -f json -s \" {chrom.name} := bchla(structure({chrom.to_qcore_string()}))\"",
+		shell=True,
+		stdout=subprocess.PIPE,
+		executable="/bin/bash",
+		universal_newlines=True)
+
+	result = json.loads(json_run.stdout)
+
+	return single_result(chrom.name, result[chrom.name]["excitation_energy"], result[chrom.name]["transition_dipole"])
 
 def grep_lines(match_str):
 	grep_run = subprocess.run(f"grep -n \"{match_str}\" neutral_result_traj_structures.pdb",
@@ -62,10 +85,9 @@ if __name__ == '__main__':
 	assert(len(start_lines) == len(end_lines))
 
 	chromophores = {}
-	energies = []
 
 	for frame in range(len(start_lines)):
-		frame_chromophore = Chromophore()
+		frame_chromophore = Chromophore(f"frame_{frame}")
 
 		for enum, line in enumerate(frame_lines[start_lines[frame]+1:end_lines[frame]]):
 			elem = re.findall(r'\S+', line)[-1]
@@ -74,9 +96,23 @@ if __name__ == '__main__':
 			frame_chromophore.append(elem, xyz)
 
 		chromophores[f"frame_{frame}"] = frame_chromophore
-		energies.append(run_qcore(frame_chromophore)["excitation_energy"])
 
-	plt.hist(energies)
-	plt.show()
+	for chrom in list(chromophores.values()):
+		chrom.to_xyz_file()
+
+	with ProcessPoolExecutor(max_workers=30) as pool:
+		results = list(pool.map(run_qcore, list(chromophores.values())))
+
+	for i in results:
+		chromophores[i.name].excitation_energy = i.energy
+		chromophores[i.name].transition_dipole = i.transition_dipole
+
+	for i in chromophores.keys():
+		print(chromophores[i].excitation_energy)
+
+
+
+
+
 
 
